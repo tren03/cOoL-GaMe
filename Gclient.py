f8c0af1a0db481c579974e678ee6a8af61d3940a
bmsce <vishnusethuraman@gmail.com>
2024-05-01 16:29:58 +0530

    2 player movement works

--- Gclient.py
+++ Gclient.py
@@ -1,72 +1,85 @@
 import pygame
+from network import Network
 
 width = 500
 height = 500
-win = pygame.display.set_mode((width,height))
-pygame.display.set_caption("client")
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
 
 clientNumber = 0
 
+
 class Player():
-    def __init__(self,x,y,width,height,color):
+    def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
-        self.rect = (self.x,self.y,self.width,self.height)
-        self.vel = 0.3 # how much to move when we press arrow keys
+        self.rect = (x,y,width,height)
+        self.vel = 3
 
-
-    def draw(self,win):
-        pygame.draw.rect(win,self.color,self.rect)
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, self.rect)
 
     def move(self):
-        keys = pygame.key.get_pressed() # returns dict of all of keys with 0 and 1 to see which key is pressed
-        if keys[pygame.K_LEFT]:
-            self.x-=self.vel
+        keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
 
         if keys[pygame.K_RIGHT]:
-            self.x+=self.vel
+            self.x += self.vel
 
         if keys[pygame.K_UP]:
-            self.y-=self.vel
+            self.y -= self.vel
 
         if keys[pygame.K_DOWN]:
-            self.y+=self.vel
+            self.y += self.vel
 
-        if(self.x < 0):
-            self.x=0
+        self.update()
 
-        if(self.y < 0):
-            self.y=0
-
-        if(self.x > width-self.width):
-            self.x=400
-
-        if(self.y > height-self.height):
-            self.y=400
-        
-        self.rect = (self.x,self.y,self.width,self.height)
+    def update(self):
+        self.rect = (self.x, self.y, self.width, self.height)
 
 
-def redrawWindow(win,player):
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]), int(str[1])
+
+
+def make_pos(tup):
+    return str(tup[0]) + "," + str(tup[1])
+
+
+def redrawWindow(win,player, player2):
     win.fill((255,255,255))
     player.draw(win)
+    player2.draw(win)
     pygame.display.update()
 
 
 def main():
-    p = Player(50,50,100,100,(255,0,0))
     run = True
+    n = Network()
+    startPos = read_pos(n.getPos())
+    p = Player(startPos[0],startPos[1],100,100,(0,255,0))
+    p2 = Player(0,0,100,100,(255,0,0))
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(60)
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        p2.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-        p.move()
-        redrawWindow(win,p)
 
+        p.move()
+        redrawWindow(win, p, p2)
 
 main()
--- network.py
+++ network.py
@@ -1,31 +1,27 @@
 import socket
 
+
 class Network:
     def __init__(self):
-        self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = "127.0.0.1"
         self.port = 5555
-        self.addr = (self.server,self.port)
-        self.id = self.connect()
-        print(self.id)
+        self.addr = (self.server, self.port)
+        self.pos = self.connect()
+
+    def getPos(self):
+        return self.pos
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()        
-        except socket.error as e:
-            print(e)
-            
+            return self.client.recv(2048).decode()
+        except:
             pass
 
-    def send(self,data):
+    def send(self, data):
         try:
             self.client.send(str.encode(data))
             return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
-
-
-n = Network()
-rec = n.send("hello from the other side")
-print(rec)
